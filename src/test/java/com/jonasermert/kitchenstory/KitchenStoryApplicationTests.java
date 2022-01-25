311be28d90f31df085f488fb4193a903f78a374a
Jonas Ermert <ermert.jonas@yahoo.de>
2022-01-25 23:34:01 +0100

    Initialized project

---
+++ src/test/java/com/jonasermert/kitchenstory/KitchenStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasermert.kitchenstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
